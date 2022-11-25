5eb19905b867b46faadf0f53f6319581594bd165
PrajaktaMhaske <99183558+PrajaktaMhaske@users.noreply.github.com>
2022-11-25 23:12:38 +0530

    Update Question8.sh

--- Question8.sh
+++ Question8.sh
@@ -1,9 +1,9 @@
 h=`date +%H`
 
 if [ $h -lt 12 ]; then
-  echo Good morning
+  echo Good Morning!
 elif [ $h -lt 18 ]; then
-  echo Good afternoon
+  echo Good Afternoon!
 else
-  echo Good evening
+  echo Good Evening!
 fi
